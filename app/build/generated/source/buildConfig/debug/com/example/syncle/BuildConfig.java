27f19e1f781a4dc0eaa3498585bdb9aff23175ba
Jian Feng <j36feng@uwaterloo.ca>
2026-05-17 21:37:23 -0400

    feat: add Android 15 16KB page compatibility properties to manifest and include room configuration assets

--- app/build/generated/source/buildConfig/debug/com/example/syncle/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/example/syncle/BuildConfig.java
@@ -10,5 +10,5 @@ public final class BuildConfig {
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";
   // Field from default config.
-  public static final String LIVEKIT_SANDBOX_ID = "gather-lhbs9u";
+  public static final String LIVEKIT_SANDBOX_ID = "syncle -lhbs9u";
 }
